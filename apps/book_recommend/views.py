965d17d9104d9d560edaaf4b7b5e3204b69a57b5
jeffer.gu <jeffery.gu@cootek.cn>
2015-10-10 16:54:30 +0800

    add Pagination

--- apps/book_recommend/views.py
+++ apps/book_recommend/views.py
@@ -1,16 +1,27 @@
 from django.template import loader,Context
 from django.http import HttpResponse
 from book_recommend.models import BookInfo
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 # Create your views here.
 def index(request):
-    books = BookInfo.objects.all()
     t = loader.get_template('book_recommend.html')
-    c = Context({'books':books})
-    return HttpResponse(t.render(c))
+    return HttpResponse(t.render())
 
 def pages(request):
-    books = BookInfo.objects.all()
-    t = loader.get_template('book_recommend_pages.html')
+    bookList = BookInfo.objects.all()
+    page_size=15
+    paginator = Paginator(bookList, page_size)
+    try:
+        page = int(request.GET.get('page','1'))
+    except ValueError:
+        page = 1
+     
+    try:
+        books = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        books = paginator.page(paginator.num_pages)         
+     
+    t = loader.get_template("book_recommend_pages.html")
     c = Context({'books':books})
     return HttpResponse(t.render(c))
